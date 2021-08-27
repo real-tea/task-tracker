7796bf10560c4c383ba9797944dd720efd7c231f
Akash Singh <Akash Singh>
2021-08-27 09:56:27 +0530

    changes in header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,21 @@
+import PropTypes from 'prop-types'
+
 import React from 'react'
 
-const Header = () => {
+const Header = ( {title} ) => {
     return (
         <header>
-            Task Tracker
+            <h1>{title}</h1>
         </header>
     )
+}
+
+Header.defaultProps = {
+    title : 'Task Tracker',
+}
+
+Header.propTypes = {
+    title : PropTypes.string.isRequired,
 }
 
 export default Header
